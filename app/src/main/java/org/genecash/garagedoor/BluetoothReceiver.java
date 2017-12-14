3fd51f0151ef0654e8014a63340c6740ed232935
Gene Cash <gene.cash@gmail.com>
2017-12-14 10:01:23 -0500

    Add Bluetooth media keys code

--- app/src/main/java/org/genecash/garagedoor/BluetoothReceiver.java
+++ app/src/main/java/org/genecash/garagedoor/BluetoothReceiver.java
@@ -10,54 +10,94 @@ import android.content.SharedPreferences;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.KeyEvent;
 
 public class BluetoothReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-        String deviceName = device.getName();
-        String address = device.getAddress();
+        Log.i("GarageDoor", "bluetooth intent: " + intent);
 
-        Log.i("GarageDoor", "bluetooth action: " + action);
-        if (deviceName != null) {
-            Log.i("GarageDoor", "bluetooth name: " + deviceName);
+        if (action == Intent.ACTION_MEDIA_BUTTON) {
+            KeyEvent event = intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+            Log.i("GarageDoor", "bluetooth key event: " + event);
+            int keyAction = event.getAction();
+            String keyString = event.getCharacters();
+            Log.i("GarageDoor", "bluetooth keys: " + keyString);
+            if (keyAction == KeyEvent.ACTION_DOWN) {
+                Log.i("GarageDoor", "bluetooth key press");
+            }
+            if (keyAction == KeyEvent.ACTION_UP) {
+                Log.i("GarageDoor", "bluetooth key release");
+            }
+            int keyCode = event.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
+                    Log.i("GarageDoor", "bluetooth key media prev");
+                    break;
+                case KeyEvent.KEYCODE_MEDIA_NEXT:
+                    Log.i("GarageDoor", "bluetooth key media next");
+                    break;
+                case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+                    Log.i("GarageDoor", "bluetooth key media play/pause");
+                    break;
+            }
+            if (event.isAltPressed()){
+                Log.i("GarageDoor", "bluetooth key alt pressed");
+            }
+            if (event.isShiftPressed()){
+                Log.i("GarageDoor", "bluetooth key shift pressed");
+            }
+            if (event.isCtrlPressed()){
+                Log.i("GarageDoor", "bluetooth key ctrl pressed");
+            }
+            return;
         }
-        Log.i("GarageDoor", "bluetooth address: " + address);
 
-        if (deviceName != null && action.equals(BluetoothDevice.ACTION_ACL_CONNECTED)) {
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-            String btName = prefs.getString(Utilities.PREFS_BT_NAME, "");
+        if (action.equals(BluetoothDevice.ACTION_ACL_CONNECTED)) {
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            if (device == null) {
+                return;
+            }
+            String deviceName = device.getName();
+            String address = device.getAddress();
+            Log.i("GarageDoor", "bluetooth address: " + address);
 
-            // determine if service is already running
-            boolean running = false;
-            if (deviceName.equals(btName)) {
-                ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-                for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-                    if (GarageDoorService.class.getName().equals(service.service.getClassName())) {
-                        running = true;
+            if (deviceName != null) {
+                Log.i("GarageDoor", "bluetooth name: " + deviceName);
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+                String btName = prefs.getString(Utilities.PREFS_BT_NAME, "");
+
+                // determine if service is already running
+                boolean running = false;
+                if (deviceName.equals(btName)) {
+                    ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+                    for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+                        if (GarageDoorService.class.getName().equals(service.service.getClassName())) {
+                            running = true;
+                        }
                     }
-                }
 
-                if (running) {
-                    // service is running, just make noise
-                    Log.i("GarageDoor", "starting service from bluetooth button");
-                    Intent service = new Intent(context, GarageDoorService.class);
-                    service.putExtra(Utilities.NOISE_FLAG, true);
-                    context.startService(service);
-                } else {
-                    // start up app so it can rouse the device and lock the screen
-                    Log.i("GarageDoor", "starting app from bluetooth button");
-                    Intent app = new Intent(context, GarageDoorApp.class);
-                    app.setAction(Intent.ACTION_MAIN);
-                    app.putExtra(Utilities.NOISE_FLAG, true);
-                    app.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    app.addCategory(Intent.CATEGORY_LAUNCHER);
-                    context.startActivity(app);
-                }
+                    if (running) {
+                        // service is running, just make noise
+                        Log.i("GarageDoor", "starting service from bluetooth button");
+                        Intent service = new Intent(context, GarageDoorService.class);
+                        service.putExtra(Utilities.NOISE_FLAG, true);
+                        context.startService(service);
+                    } else {
+                        // start up app so it can rouse the device and lock the screen
+                        Log.i("GarageDoor", "starting app from bluetooth button");
+                        Intent app = new Intent(context, GarageDoorApp.class);
+                        app.setAction(Intent.ACTION_MAIN);
+                        app.putExtra(Utilities.NOISE_FLAG, true);
+                        app.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        app.addCategory(Intent.CATEGORY_LAUNCHER);
+                        context.startActivity(app);
+                    }
 
-                // we can't just sleep
-                new Handler().postDelayed(new RestoreBluetooth(), 1 * Utilities.MILLISECONDS);
+                    // we can't just sleep
+                    new Handler().postDelayed(new RestoreBluetooth(), 1 * Utilities.MILLISECONDS);
+                }
             }
         }
     }
--- app/src/main/java/org/genecash/garagedoor/GarageDoorOpen.java
+++ app/src/main/java/org/genecash/garagedoor/GarageDoorOpen.java
@@ -2,6 +2,7 @@ package org.genecash.garagedoor;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -20,14 +21,14 @@ import static org.genecash.garagedoor.Utilities.RESPONSE;
 import static org.genecash.garagedoor.Utilities.isDataEnabled;
 import static org.genecash.garagedoor.Utilities.isNetworkAvailable;
 import static org.genecash.garagedoor.Utilities.log;
+import static org.genecash.garagedoor.Utilities.logExcept;
 import static org.genecash.garagedoor.Utilities.setDataEnabled;
 import static org.genecash.garagedoor.Utilities.sleep;
 
 public class GarageDoorOpen extends Activity {
     String hostname;
     int port;
-    SSLSocketFactory sslSocketFactory;
-    Socket sock = null;
+    ContentResolver cr;
     boolean manageData;
     String command = "OPENCLOSE";
 
@@ -46,17 +47,21 @@ public class GarageDoorOpen extends Activity {
         port = prefs.getInt(Utilities.PREFS_PORT, 17000);
         manageData = prefs.getBoolean(Utilities.PREFS_DATA, true);
 
+        cr = getContentResolver();
+        if (!isDataEnabled(cr) && manageData) {
+            setDataEnabled(cr, true);
+        }
+
         // stop background service process
         sendBroadcast(new Intent(Utilities.ACTION_STOP));
 
-        sslSocketFactory = Utilities.initSSL(this);
         new NetworkSession().execute();
     }
 
     @Override
     protected void onDestroy() {
         if (manageData) {
-            setDataEnabled(getContentResolver(), false);
+            setDataEnabled(cr, false);
         }
         super.onDestroy();
     }
@@ -64,35 +69,43 @@ public class GarageDoorOpen extends Activity {
     class NetworkSession extends AsyncTask<Void, String, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
+            Socket sock;
+            Context ctx = getApplicationContext();
+            SSLSocketFactory sslSocketFactory = Utilities.initSSL(ctx);
             while (true) {
                 try {
                     // connect
                     sock = sslSocketFactory.createSocket(hostname, port);
+
                     // we should get the proper connection response
                     BufferedReader buffRdr = new BufferedReader(new InputStreamReader(sock.getInputStream(), "ASCII"));
                     String response = buffRdr.readLine();
                     if (!response.equals(RESPONSE)) {
                         log("invalid response: " + response);
+                        sock.close();
+                        continue;
                     }
+
                     // send command
                     sock.getOutputStream().write((command + "\n").getBytes());
+
                     // we should get the proper command response
                     response = buffRdr.readLine();
                     if (response.equals(command + " DONE")) {
+                        // yay! we're done
                         log("OpenDoor response received");
                         publishProgress(null);
                         return null;
                     } else {
+                        // boo!
                         log("invalid OpenDoor response: " + response);
+                        sock.close();
+                        continue;
                     }
                 } catch (IOException e) {
-                    ContentResolver cr = getContentResolver();
-                    log("Network: " + isNetworkAvailable(getApplicationContext()));
+                    logExcept("OpenDoor", e);
+                    log("Network: " + isNetworkAvailable(ctx));
                     log("Data: " + isDataEnabled(cr));
-                    if (!isDataEnabled(cr) && manageData) {
-                        setDataEnabled(cr, true);
-                        sleep(10 * MILLISECONDS);
-                    }
                 }
                 // don't spam the living hell out of the logs
                 sleep(MILLISECONDS / 2);
