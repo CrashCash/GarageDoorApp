2d562c9173f44b0f8e42217c9062c673d12ed78d
Gene Cash <gene.cash@gmail.com>
2019-01-20 17:13:19 -0500

    Arm & button applets don't need a screen/theme. Also do networking inline and go rid of asynctask.

--- app/src/main/java/org/genecash/garagedoor/GarageDoorOpen.java
+++ app/src/main/java/org/genecash/garagedoor/GarageDoorOpen.java
@@ -1,12 +1,11 @@
 package org.genecash.garagedoor;
 
 import android.app.Activity;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.text.format.DateUtils;
 
@@ -18,11 +17,8 @@ import java.net.Socket;
 import javax.net.ssl.SSLSocketFactory;
 
 import static org.genecash.garagedoor.Utilities.RESPONSE;
-import static org.genecash.garagedoor.Utilities.isDataEnabled;
-import static org.genecash.garagedoor.Utilities.isNetworkAvailable;
 import static org.genecash.garagedoor.Utilities.log;
 import static org.genecash.garagedoor.Utilities.logExcept;
-import static org.genecash.garagedoor.Utilities.setDataEnabled;
 import static org.genecash.garagedoor.Utilities.setupLogging;
 import static org.genecash.garagedoor.Utilities.sleep;
 import static org.genecash.garagedoor.Utilities.stopLogging;
@@ -31,11 +27,11 @@ public class GarageDoorOpen extends Activity {
     String hostname;
     int port;
     String password;
-    ContentResolver cr;
     Context ctx = this;
     boolean manageData;
     String command = "OPENCLOSE";
     String logname = "arm";
+    Socket sock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,77 +48,51 @@ public class GarageDoorOpen extends Activity {
         manageData = prefs.getBoolean(Utilities.PREFS_DATA, true);
         password = prefs.getString(Utilities.PREFS_KEYSTORE_PASSWORD, "");
 
-        cr = getContentResolver();
-        if (!isDataEnabled(cr) && manageData) {
-            setDataEnabled(cr, true);
-        }
-
         // stop background service process
         sendBroadcast(new Intent(Utilities.ACTION_STOP));
 
-        new NetworkSession().execute();
-    }
+        // disable NetworkOnMainThreadException
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitNetwork().build();
+        StrictMode.setThreadPolicy(policy);
 
-    @Override
-    protected void onDestroy() {
-        if (manageData && isDataEnabled(cr)) {
-            setDataEnabled(cr, false);
-        }
-        stopLogging();
-        super.onDestroy();
-    }
+        SSLSocketFactory sslSocketFactory = Utilities.initSSL(ctx, password);
+        while (true) {
+            try {
+                // connect
+                sock = sslSocketFactory.createSocket(hostname, port);
 
-    class NetworkSession extends AsyncTask<Void, String, Void> {
-        @Override
-        protected Void doInBackground(Void... voids) {
-            Socket sock;
-            SSLSocketFactory sslSocketFactory = Utilities.initSSL(ctx, password);
-            while (true) {
-                try {
-                    // connect
-                    sock = sslSocketFactory.createSocket(hostname, port);
-
-                    // we should get the proper connection response
-                    BufferedReader buffRdr = new BufferedReader(new InputStreamReader(sock.getInputStream(), "ASCII"));
-                    String response = buffRdr.readLine();
-                    if (!response.equals(RESPONSE)) {
-                        log("invalid response: " + response);
-                        sock.close();
-                        continue;
-                    }
-
-                    // send command
-                    sock.getOutputStream().write((command + "\n").getBytes());
-
-                    // we should get the proper command response
-                    response = buffRdr.readLine();
-                    if (response.equals(command + " DONE")) {
-                        // yay! we're done
-                        break;
-                    } else {
-                        // boo!
-                        log("invalid OpenDoor response: " + response);
-                        sock.close();
-                        continue;
-                    }
-                } catch (IOException e) {
-                    logExcept(e);
-                    log("Network: " + isNetworkAvailable(ctx));
-                    log("Data: " + isDataEnabled(cr));
-                    if (!isDataEnabled(cr) && manageData) {
-                        setDataEnabled(cr, true);
-                    }
+                // we should get the proper connection response
+                BufferedReader buffRdr = new BufferedReader(new InputStreamReader(sock.getInputStream(), "ASCII"));
+                String response = buffRdr.readLine();
+                if (!response.equals(RESPONSE)) {
+                    log("invalid response: " + response);
+                    sock.close();
+                    continue;
                 }
-                // don't spam the living hell out of the logs
-                sleep(DateUtils.SECOND_IN_MILLIS / 2);
+
+                // send command
+                sock.getOutputStream().write((command + "\n").getBytes());
+
+                // we should get the proper command response
+                response = buffRdr.readLine();
+                if (response.equals(command + " DONE")) {
+                    // yay! we're done
+                    break;
+                } else {
+                    // boo!
+                    log("invalid OpenDoor response: " + response);
+                    sock.close();
+                    continue;
+                }
+            } catch (IOException e) {
+                logExcept(e);
             }
-            publishProgress();
-            return null;
+            // don't spam the living hell out of the logs
+            sleep(DateUtils.SECOND_IN_MILLIS / 2);
         }
 
-        @Override
-        protected void onProgressUpdate(String... values) {
-            finish();
-        }
+        stopLogging();
+        finish();
     }
 }
+
